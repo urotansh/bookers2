3c114ccf1549c6fac8c2a92ee68a4cb346223f0d
urotansh <114655455+urotansh@users.noreply.github.com>
2022-10-14 03:03:18 +0000

    [Add] 4-27 投稿画面・投稿機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,13 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def show
@@ -7,4 +15,12 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+  
 end
